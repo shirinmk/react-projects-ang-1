98c9075425485047650bc1ea73bdb7aba6110ac9
Shirin Kalantari <sh_manoochehri@yahoo.com>
2023-09-07 11:10:28 -0700

    reusable card with using styles

--- practice-prop/src/components/App.js
+++ practice-prop/src/components/App.js
@@ -4,11 +4,10 @@ import Card from "./Card";
 function App() {
   return (
     <div>
-    <h1>My contacts</h1>
+    <h1 className="heading">My Contacts</h1>
       <Card
         name="Beyonce"
         image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        alt="avatar_img"
         phone="+123 456 789"
         email="b@beyonce.com"
       />
@@ -16,14 +15,14 @@ function App() {
       <Card
         name="Jack Bauer"
         image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        alt="avatar_img"
+     
         phone="+987 654 321"
         email="jack@nowhere.com"
       />
       <Card
         name="Chuck Norris"
         image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        alt="avatar_img"
+    
         phone="+918 372 574"
         email="gmail@chucknorris.com"
       />
--- practice-prop/src/components/Card.js
+++ practice-prop/src/components/Card.js
@@ -3,12 +3,23 @@ import React from "react";
 function Card(props) {
   // console.log(props)
   return (
-    <duv>
-      <h2>{props.name}</h2>
-      <img src={props.image} alt={props.alt} />
-      <p>{props.phone}</p>
-      <p>{props.email}</p>
-    </duv>
+    <div>
+    
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img
+        className="circle-img"
+          src={props.image}
+          alt="avatar_img"
+        />
+      </div>
+      <div className="bottom">
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  </div>
   );
 }
 
