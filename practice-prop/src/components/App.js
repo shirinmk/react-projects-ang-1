7366d8ecfc98a8b367d235a863df6046b1604bc2
Shirin Kalantari <sh_manoochehri@yahoo.com>
2023-09-07 09:23:57 -0700

    create reusable card to put diff info props

--- practice-prop/src/components/App.js
+++ practice-prop/src/components/App.js
@@ -1,33 +1,32 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
-      <h1>My Contacts</h1>
-
-      <h2>Beyonce</h2>
-      <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    <h1>My contacts</h1>
+      <Card
+        name="Beyonce"
+        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
         alt="avatar_img"
+        phone="+123 456 789"
+        email="b@beyonce.com"
       />
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
 
-      <h2>Jack Bauer</h2>
-      <img
-        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      <Card
+        name="Jack Bauer"
+        image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
         alt="avatar_img"
+        phone="+987 654 321"
+        email="jack@nowhere.com"
       />
-      <p>+987 654 321</p>
-      <p>jack@nowhere.com</p>
-
-      <h2>Chuck Norris</h2>
-      <img
-        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      <Card
+        name="Chuck Norris"
+        image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
         alt="avatar_img"
+        phone="+918 372 574"
+        email="gmail@chucknorris.com"
       />
-      <p>+918 372 574</p>
-      <p>gmail@chucknorris.com</p>
     </div>
   );
 }
