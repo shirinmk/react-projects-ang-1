42f9f46aadcf4e84feb69167eed13e606ae8b49c
Shirin Kalantari <sh_manoochehri@yahoo.com>
2023-09-07 12:00:28 -0700

    using array to fill values of custom html tag

---
+++ props/2-reuse-style/src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Card(props) {
+  // console.log(props)
+  return (
+    <div>
+    
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img
+        className="circle-img"
+          src={props.image}
+          alt="avatar_img"
+        />
+      </div>
+      <div className="bottom">
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Card;
