670f4de9f619bcca368154eaa4accecb44e17b63
Shirin Kalantari <sh_manoochehri@yahoo.com>
2023-09-12 11:01:05 -0700

    learning map reduce filter

--- practice-prop/src/components/App.js
+++ practice-prop/src/components/App.js
@@ -1,31 +1,7 @@
 import React from "react";
-import Entry from "./Entry";
-//import array of info
-import emojipedia from "../emojipedia";
-function App() {
-  console.log(emojipedia);
-  console.log(emojipedia[0])
-  function createEntry(emoji){
-    return(
-      <Entry image={emoji.emoji} name={emoji.name} meaning={emoji.meaning} />
-    )
-  }
-  return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
 
-      <dl className="dictionary">
-        {/* <Entry
-        image = {emojipedia[0].emoji}
-        name= {emojipedia[0].name}
-        meaning={emojipedia[0].meaning}
-         /> */}
-        { emojipedia.map(createEntry)}
-      </dl>
-    </div>
-  );
+function App() {
+  return <div></div>;
 }
 
 export default App;
